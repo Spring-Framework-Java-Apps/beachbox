6ee586572f44666b29b76c8a58be1e0dbc43ab53
phasenraum2010 <thomas@woehlke.org>
2015-02-14 11:19:52 +0100

    Added VinylType.java

---
+++ src/main/java/org/woehlke/beachbox/entities/VinylType.java
@@ -0,0 +1,15 @@
+package org.woehlke.beachbox.entities;
+
+/**
+ * Created by tw on 14.02.15.
+ */
+public enum VinylType {
+    CD,
+    CD_SAMPLER,
+    LP,
+    LP_SAMPLER,
+    SINGLES,
+    DVD,
+    MC,
+    VIDEO
+}
